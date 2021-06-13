d50a00a493db63329a2ae5a106af04c99639f967
E31190731 <65715169+E31190731@users.noreply.github.com>
2021-06-13 09:09:54 +0700

    Upload

---
+++ 12/ci/application/views/crud/home_mahasiswa.php
@@ -0,0 +1,33 @@
+<div class="card shadow mb-4">
+	<div class="card-header py-3">
+	<h6 class="m-0 fint-weight-bold text-primary">Data Tables User</h6></div>
+	<div class="card-body"><div class="table-responsive">
+		<table class="table table-bordered" id="dataTabe" width="100%" cellspacing="0">
+		<thead>
+		<tr><th>No</th>
+			<th>Username</th>
+			<th>Grup</th>
+			<th></th></tr>
+		</thead>
+		<tfoot>
+		<tr><th>No</th>
+			<th>Username</th>
+			<th>Grup</th>
+			<th></th></tr>
+		</tfoot>
+		<tbody>
+		<?php $no = 1;
+			  foreach($user as $baris){
+		?>
+		<tr><td><?php echo $no++ ?></td>
+			<td><?php echo $baris -> username; ?></td>
+			<td><?php echo $baris -> grup; ?></td>
+			<td><a href="<?php echo base_url('index.php/Mahasiswa/edit/'.$baris->id); ?>" class="fa fa-edit">&nbsp;
+				<a href="<?php echo base_url('index.php/Mahasiswa/hapus/'.$baris->id); ?>" class="fa fa-times"></td></tr>
+		<?php } ?>
+		</tbody>
+		</table>
+		<a href="<?php echo base_url('index.php/Mahasiswa/tambah')?>" class="btn btn-success btn-icon-split">
+		<span class="text">Tambah Data</span>
+		</a>
+</div></div></div>
